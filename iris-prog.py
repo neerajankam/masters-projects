cce43662b666d2a510a242928a95918698da3a99
group12ML <ankam.neeraj@gmail.com>
2018-02-09 15:28:23 -0500

    Logistic regression on the iris dataset using scikit learn

--- iris-prog.py
+++ iris-prog.py
@@ -14,13 +14,14 @@ data = pd.DataFrame(data=iris.data,columns=iris.feature_names)
 shuffled_data,shuffled_target=shuffle(data,iris.target)
 
 from sklearn.model_selection import train_test_split,cross_val_score
-
 from sklearn import linear_model
+from sklearn.linear_model import LogisticRegression
 #Splitting the data into training and test data. 20% of the data is used for testing purpose
 X_train,X_test,y_train,y_test = train_test_split(shuffled_data,shuffled_target,test_size=0.2)
 #Fitting a logistic regression model to our data and doing 5-fold cross validation
-log_reg= linear_model.LogisticRegressionCV(cv=5).fit(X_train,y_train)
+log_reg= linear_model.LogisticRegression().fit(X_train,y_train)
 
 y_predicted = log_reg.predict(X_test)
+scores= cross_val_score(log_reg,X_test,y_test,cv=5)
 #Reporting the accuracy of our prediction by comparing the actual and predicted y values
-print("Accuracy score:",accuracy_score(y_test,y_predicted))
+print("Accuracy score:",scores.mean())
